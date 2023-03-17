0cf946f586c059aa5d2630c47cd8340a906cc047
vokeh22 <muthamahkelvin@gmail.com>
2023-03-17 04:27:09 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main part
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("char");
+	printf("int");
+	printf("float");
+	printf("double);
+	return (0);
+}
