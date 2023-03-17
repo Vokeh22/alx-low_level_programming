abfd7e6f195f252c783d2615dba7ffbef19d6c4b
vokeh22 <muthamahkelvin@gmail.com>
2023-03-17 03:10:16 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+
+/**
+ * main - starts
+ *
+ * Return: zero
+ */
 int main (void)
 {
-	puts(\"Programming is like building a multilingual puzzle \n");
-	return 0;
-}
+	puts("\"Programming is like building a multilingual puzzle \n");
+	return (0);
