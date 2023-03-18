314204bf6537306ef1dbc71f14a6241fff175b76
vokeh22 <muthamahkelvin@gmail.com>
2023-03-17 19:13:12 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,12 +11,12 @@ int main(void)
 
 	for (c = 'a'; c <= 'z'; ++c)
 	{
-	putchar(c);
-	}
-	for (c = 'A'; c <= 'Z'; ++c)
-	{
-		putchar(c);
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
 	}
 	putchar('\n');
+
 	return (0);
 }
